cb4746337897a9cdedfb346904214abc8ad8a7f0
chloeb-smith <158786659+chloeb-smith@users.noreply.github.com>
2024-10-18 14:40:34 -0400

    Add files via upload

---
+++ Automobile.java
@@ -0,0 +1,26 @@
+public class Automobile extends LandTransportation {
+
+    private String make;
+    private String model;
+    private int year;
+
+    public Automobile(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, String make, String model, int year) {
+        super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
+
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake(){return make;}
+    public void setMake(String make){this.make = make;}
+
+    public String getModel(){return model;}
+    public void setModel(String model){this.model = model;}
+
+    public int getYear(){return year;}
+    public void setYear(int year){this.year = year;}
+
+    @Override
+    public String toString(){return ".";}
+}
