4fb28751dfbb0a104220f9d7f48cf3897d0ff215
chloeb-smith <158786659+chloeb-smith@users.noreply.github.com>
2024-10-18 14:49:11 -0400

    Add files via upload

---
+++ RoundThreeClasses/Bus.java
@@ -0,0 +1,17 @@
+public class Bus extends LandTransportation {
+
+    private int routeNum;
+
+    public Bus(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, int routeNum) {
+        super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
+
+        this.routeNum = routeNum;
+    }
+
+    public int getRouteNum(){return routeNum;}
+    public void setRouteNum(int routeNum){this.routeNum = routeNum;}
+
+    @Override
+    public String toString(){return ".";}
+
+}
