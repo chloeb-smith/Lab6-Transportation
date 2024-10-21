9db9b6137f7228bfdc9700f63ec9f937693e77c4
chloeb-smith <158786659+chloeb-smith@users.noreply.github.com>
2024-10-21 10:41:52 -0400

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,20 @@
+public class Bike extends LandTransportation {
+    private String brand;
+    private int numGears;
+
+    public Bike(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, String brand, int numGears){
+        super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
+
+        this.brand = brand;
+        this.numGears = numGears;
+    }
+
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand = brand;}
+
+    public int getNumGears(){return numGears;}
+    public void setNumGears(int numGears){this.numGears = numGears;}
+
+    @Override
+    public String toString(){return ".";}
+}
