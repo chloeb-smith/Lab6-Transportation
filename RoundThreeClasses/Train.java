b930de2a9d1eebb6a864628ca382c8d5ccb39e5c
tas8077 <tas8077@uncw.edu>
2024-10-25 14:49:09 -0400

    Update Train.java

--- RoundThreeClasses/Train.java
+++ RoundThreeClasses/Train.java
@@ -1,14 +1,29 @@
 public class Train extends LandTransportation {
 
+    // Class Variables
     private String fuelType;
 
+    // Constructor
     public Train(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, String fuelType) {
         super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
 
         this.fuelType = fuelType;
     }
 
+    // Getters
+    
+    /** 
+    * Gets the fuel type
+    * @return fuelType
+    */ 
     public String getFuelType(){return fuelType;}
+
+    // Setters
+
+    /** 
+    * Setters the fuel type
+    * @param fuelType
+    */ 
     public void setFuelType(String fuelType){this.fuelType = fuelType;}
 
     /**
