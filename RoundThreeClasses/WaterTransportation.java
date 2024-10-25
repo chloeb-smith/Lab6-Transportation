9a8a2887cdb7ab56a945a8e018f62b013ff4b138
tas8077 <tas8077@uncw.edu>
2024-10-25 14:50:15 -0400

    Update WaterTransportation.java

--- RoundThreeClasses/WaterTransportation.java
+++ RoundThreeClasses/WaterTransportation.java
@@ -1,13 +1,29 @@
 public class WaterTransportation extends Transportation {
 
+    // Class Variables
     private String waterType;
 
+    // Constructor
     public WaterTransportation(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String waterType){
         super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions);
 
         this.waterType = waterType;
     }
+
+    // Getters
+    
+    /** 
+    * Gets the water type
+    * @return waterType
+    */ 
     public String getWaterType(){return waterType;}
+
+    // Setters
+    
+    /** 
+    * Sets the water type
+    * @param waterType
+    */ 
     public void setWaterType(String waterType){this.waterType = waterType;}
 
     /**
