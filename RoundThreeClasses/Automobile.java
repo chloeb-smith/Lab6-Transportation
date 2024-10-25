15f0c600b713515bbd5eeb76be6d12020a952684
tas8077 <tas8077@uncw.edu>
2024-10-25 14:27:55 -0400

    Update Automobile.java

--- RoundThreeClasses/Automobile.java
+++ RoundThreeClasses/Automobile.java
@@ -1,9 +1,10 @@
 public class Automobile extends LandTransportation {
-
+    // Class Attributes
     private String make;
     private String model;
     private int year;
 
+    // Constructor
     public Automobile(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, String make, String model, int year) {
         super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
 
@@ -11,16 +12,48 @@ public class Automobile extends LandTransportation {
         this.model = model;
         this.year = year;
     }
+    // Getters
     
+    /**
+    * Gets the make
+    * @return make 
+    */
     public String getMake(){return make;}
+
+    /**
+    * Gets the model
+    * @return model 
+    */
+    public String getModel(){return model;}
+
+    /**
+    * Gets the year
+    * @return year 
+    */
+    public int getYear(){return year;}
+
+    // Setters
+
+    /**
+    * Sets the make
+    * @param make 
+    */
     public void setMake(String make){this.make = make;}
 
-    public String getModel(){return model;}
+    /**
+    * Sets the model
+    * @param model 
+    */
     public void setModel(String model){this.model = model;}
 
-    public int getYear(){return year;}
+    /**
+    * Sets the year
+    * @param year 
+    */
     public void setYear(int year){this.year = year;}
 
+
+    
     /**
     * Creates a string representation of the methods output
     * @return a string
