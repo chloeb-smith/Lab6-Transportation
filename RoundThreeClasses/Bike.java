dae5c740ab321f4d5c65204a9d182b6dba1e92f0
tas8077 <tas8077@uncw.edu>
2024-10-25 14:32:58 -0400

    Update Bike.java

--- RoundThreeClasses/Bike.java
+++ RoundThreeClasses/Bike.java
@@ -1,7 +1,9 @@
 public class Bike extends LandTransportation {
+    // Class Attributes
     private String brand;
     private int numGears;
 
+    // Constructor
     public Bike(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, String brand, int numGears){
         super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
 
@@ -9,10 +11,32 @@ public class Bike extends LandTransportation {
         this.numGears = numGears;
     }
 
+    // Getters
+
+    /** 
+    * Gets the brand
+    * @return brand
+    */
     public String getBrand(){return brand;}
+
+    /** 
+    * Gets the number of gears
+    * @return numGears
+    */
+    public int getNumGears(){return numGears;}
+
+    // Setters
+
+    /** 
+    * Sets the brand
+    * @param brand
+    */
     public void setBrand(String brand){this.brand = brand;}
 
-    public int getNumGears(){return numGears;}
+    /** 
+    * Sets the number of gears
+    * @param numGears
+    */
     public void setNumGears(int numGears){this.numGears = numGears;}
 
     /**
