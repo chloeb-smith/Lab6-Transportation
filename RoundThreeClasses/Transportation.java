e5bc9378c21fce7c66a87d60c32f2882310c3c05
tas8077 <tas8077@uncw.edu>
2024-10-25 14:58:19 -0400

    Update Transportation.java

--- RoundThreeClasses/Transportation.java
+++ RoundThreeClasses/Transportation.java
@@ -1,4 +1,6 @@
 public class Transportation {
+
+    // Class Variables
     private String name;
     private double averageSpeed;
     private double cost;
@@ -8,6 +10,7 @@ public class Transportation {
     private int maxPassengers;
     private String travelInstructions;
 
+    // Constructor
     public Transportation(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions){
         this.name = name;
         this.averageSpeed = averageSpeed;
@@ -19,33 +22,120 @@ public class Transportation {
         this.purchased = purchased;
     }
 
+    // Getters
+
+    /**
+    * Gets the name
+    * @return name
+    */
     public String getName(){return name;}
+
+    /**
+    * Gets the average speed
+    * @return averageSpeed
+    */
+    public double getAverageSpeed(){return averageSpeed;}
+
+    /**
+    * Gets the cost
+    * @return cost
+    */
+    public double getCost(){return cost;}
+
+    /**
+    * Gets the purchase type
+    * @return purchaseType
+    */
+    public String getPurchaseType(){return purchaseType;}
+
+    /**
+    * Gets the purchased boolean
+    * @return purchased
+    */
+    public boolean getPurchased(){return purchased;}
+
+    /**
+    * Gets the number purchased
+    * @return numPurchased
+    */
+    public int getNumPurchased(){return numPurchased;}
+
+    /**
+    * Gets the maxiumum passengers 
+    * @return maxPassengers
+    */
+    public int getMaxPassengers(){return maxPassengers;}
+
+    /**
+    * Gets the travel instructions
+    * @return travelInstructions
+    */
+    public String getTravelInstructions(){return travelInstructions;}
+
+    /**
+    * Gets the total cost
+    * @return total cost
+    */
+    public double getComputedTicketCost(){return cost * numPurchased;}
+
+
+    // Setters
+
+    /**
+    * Sets the name
+    * @param name
+    */
     public void setName(String name){this.name = name;}
 
-    public double getAverageSpeed(){return averageSpeed;}
+    /**
+    * Sets the average speed
+    * @param average speed
+    */
     public void setAverageSpeed(double averageSpeed){this.averageSpeed = averageSpeed;}
 
-    public double getCost(){return cost;}
+    /**
+    * Sets the cost
+    * @param cost
+    */
     public void setCost(double cost){this.cost = cost;}
 
-    public String getPurchaseType(){return purchaseType;}
+    /**
+    * Sets the purchase type
+    * @param purchase type
+    */
     public void setPurchaseType(String purchaseType){this.purchaseType = purchaseType;}
 
-    public boolean getPurchased(){return purchased;}
+    /**
+    * Sets the purchased bool
+    * @param purchased
+    */
     public void setPurchased(boolean purchased){this.purchased = purchased;}
 
-    public int getNumPurchased(){return numPurchased;}
+    /**
+    * Sets the number purchased
+    * @param numPurchased
+    */
     public void setNumPurchased(int numPurchased){this.numPurchased = numPurchased;}
 
-    public int getMaxPassengers(){return maxPassengers;}
+    /**
+    * Sets the maximum passengers
+    * @param maxPassengers
+    */
     public void setMaxPassengers(int maxPassengers){this.maxPassengers = maxPassengers;}
 
-    public String getTravelInstructions(){return travelInstructions;}
+    /**
+    * Sets the travel instructions
+    * @param travelInstructions
+    */
     public void setTravelInstructions(String travelInstructions){this.travelInstructions = travelInstructions;}
 
-    public double getComputedTicketCost(){return cost * numPurchased;}
+    /**
+    * Sets the total cost
+    * @param cost
+    */
     public double getComputedRentalCost(){return (Math.ceil((double) numPurchased / maxPassengers)) * cost;}
-
+    
+    
     /**
     * Creates a string representation of the methods output
     * @return a string
