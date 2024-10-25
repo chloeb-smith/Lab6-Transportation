2bbfc3f90d7b496c1e6172b409d8f5a787c21603
tas8077 <tas8077@uncw.edu>
2024-10-25 14:36:36 -0400

    Update Bus.java

--- RoundThreeClasses/Bus.java
+++ RoundThreeClasses/Bus.java
@@ -1,14 +1,28 @@
 public class Bus extends LandTransportation {
-
+    // Class Variables
     private int routeNum;
 
+    // Constructor
     public Bus(String name, double averageSpeed, double cost, String purchaseType, boolean purchased, int numPurchased, int maxPassengers, String travelInstructions, String terrain, int numWheels, int routeNum) {
         super(name, averageSpeed, cost, purchaseType, purchased, numPurchased, maxPassengers, travelInstructions, terrain, numWheels);
 
         this.routeNum = routeNum;
     }
 
+    // Getters
+
+    /** 
+    * Gets the route number
+    * @return routeNum
+    */ 
     public int getRouteNum(){return routeNum;}
+
+    // Setters
+
+    /** 
+    * Sets the route number
+    * @param routeNum
+    */ 
     public void setRouteNum(int routeNum){this.routeNum = routeNum;}
 
     /**
