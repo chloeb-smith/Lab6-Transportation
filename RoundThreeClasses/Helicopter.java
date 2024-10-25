dfae3ef6259e2e782a39995a919ab375805d32cf
tas8077 <tas8077@uncw.edu>
2024-10-25 14:22:51 -0400

    Update Helicopter.java

--- RoundThreeClasses/Helicopter.java
+++ RoundThreeClasses/Helicopter.java
@@ -15,7 +15,10 @@ public class Helicopter extends AirTransportation{
     //Mutator Methods (setters)
     public void setNumBlades(int num) {this.numBlades = num;}
 
-
+    /**
+    * Creates a string representation of the methods output
+    * @return a string
+    */
     @Override
     public String toString(){
         return String.format(
